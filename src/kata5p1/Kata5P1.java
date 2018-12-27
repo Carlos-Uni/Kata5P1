3125c9ce4298519ca8fe508dfd3179ba9149041a
Carlos <>
2018-12-27 18:46:08 +0000

    Tercera version Kata5P1

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -3,27 +3,36 @@ package kata5p1;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 public class Kata5P1 {
 
     public static void main(String[] args) throws SQLException {
-        createNewTable("jdbc:sqlite:Kata5.db", "EMAIL");
-    }   
-
-public static void createNewTable(String url, String table) {
-        // Instrucción SQL para crear nueva tabla
-        String sql= "CREATE TABLE IF NOT EXISTS " + table + " (\n"
-            + "id integer PRIMARY KEY AUTOINCREMENT,\n"
-            + "mail text NOT NULL);";
-        try (Connection conn = DriverManager.getConnection(url);
-                Statement stmt = conn.createStatement()) {
-            // Se crea la nueva tabla
-            stmt.execute(sql);
-            System.out.println("Tabla creada");
+        String table = "EMAIL";
+        List<String> listMails = new MailListReader().read("C:\\Users\\charl\\Documents\\NetBeansProjects\\Kata5\\emails.txt");
+        String sql = "INSERT INTO " + table + "(mail)" + " VALUES(?)";
+        
+        try (Connection conn= connect();
+            PreparedStatement pstmt= conn.prepareStatement(sql)) {
+            for (String email : listMails) {
+                pstmt.setString(1, email);
+                pstmt.executeUpdate();
+            }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+    }   
+
+    public static Connection connect() {
+        Connection conn= null;
+        try {
+            conn= DriverManager.getConnection("jdbc:sqlite:Kata5.db");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
     }
 }
